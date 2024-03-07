7109bc81ff2fadf3eba1703a13fcfa3cc9bb314f
eric capiz <ericcapiz@gmail.com>
2024-03-07 17:51:47 -0600

    navbar/footer/metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,11 +1,16 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Navbar from "@/components/NavBar";
+import Footer from "@/components/Footer";
 
 const inter = Inter({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-  title: "Dev Jobs",
+  title: {
+    default: "Dev Jobs",
+    template: "%s | Dev Jobs",
+  },
   description: "Search jobs that are looking for developers like yourself!",
 };
 
@@ -16,7 +21,11 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={`${inter.className} min-w-[350px]`}>{children}</body>
+      <body className={`${inter.className} min-w-[350px]`}>
+        <Navbar />
+        {children}
+        <Footer />
+      </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,6 +3,7 @@ import JobResults from "@/components/JobResults";
 import JobListItem from "@/components/ui/JobListItem";
 import H1 from "@/components/ui/h1";
 import { JobFilterValues } from "@/lib/validation";
+import { Metadata } from "next";
 
 interface PageProps {
   searchParams: {
@@ -10,6 +11,33 @@ interface PageProps {
     type?: string;
     location?: string;
     remote?: string;
+  };
+}
+
+function getTitle({ q, type, location, remote }: JobFilterValues) {
+  const titlePrefix = q
+    ? `${q} jobs`
+    : type
+      ? `${type} developer jobs`
+      : remote
+        ? "Remote developer jobs"
+        : "All developer jobs";
+
+  const titleSuffix = location ? ` in ${location}` : "";
+
+  return `${titlePrefix}${titleSuffix}`;
+}
+
+export function generateMetadata({
+  searchParams: { q, type, location, remote },
+}: PageProps): Metadata {
+  return {
+    title: `${getTitle({
+      q,
+      type,
+      location,
+      remote: remote === "true",
+    })} | Dev Jobs`,
   };
 }
 
@@ -25,7 +53,7 @@ export default async function Home({
   return (
     <main className="m-auto my-10 max-w-5xl space-y-10 px-3 ">
       <div className="spacey-y-5 text-center">
-        <H1>Developer Jobs</H1>
+        <H1>{getTitle(filterValues)}</H1>
         <p className="text-muted-foreground">Find your dream job</p>
       </div>
       <section className="flex flex-col gap-4 md:flex-row ">
