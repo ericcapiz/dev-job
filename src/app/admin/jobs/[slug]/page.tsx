d216d38ae9ecb8d95c14a1998b4ad14463e83f8c
eric capiz <ericcapiz@gmail.com>
2024-03-11 18:07:37 -0500

    delete/approve job functionality started

---
+++ src/app/admin/jobs/[slug]/page.tsx
@@ -0,0 +1,20 @@
+import JobPage from "@/components/JobPage";
+import prisma from "@/lib/prisma";
+import { notFound } from "next/navigation";
+
+interface PageProps {
+  params: { slug: string };
+}
+
+export default async function Page({ params: { slug } }: PageProps) {
+  const job = await prisma.job.findUnique({
+    where: { slug },
+  });
+  if (!job) notFound();
+
+  return (
+    <main className="m-auto my-10 max-w-5xl flex-col items-center gap-5 px-3 md:flex-row md:items-start">
+      <JobPage job={job} />
+    </main>
+  );
+}
